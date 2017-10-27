b679abb96d929cd4b75dfa3e49fa53dad8449ad5
Michael S. Hearn <michael@byteslinger.net>
2017-10-27 13:16:48 -0700

    Initial Commit

---
+++ PlaySounds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlaySounds
+//
+//  Created by Michael S. Hearn on 10/27/17.
+//  Copyright © 2017 ByteSlinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
